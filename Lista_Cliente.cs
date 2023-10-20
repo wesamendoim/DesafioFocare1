df6318de1d7170cdc542f37bab206163f24ab5ea
wesamendoim <wesleyvbnardino@hotmail.com>
2023-10-20 15:28:52 -0300

    Add project files.

---
+++ Lista_Cliente.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioFocare1
+{   
+
+   public class ListaCliente
+    {
+        public List<int> Idade()
+        {
+            return List<>;
+        }
+        public List<string> Sexo()
+        {
+
+        }
+    }
+}
